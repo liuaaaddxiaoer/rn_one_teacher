c6803f2d8542a47e5af7882b95a55ead778e25f2
liuaaaddxiaoer <oneenddi@163.com>
2018-01-26 19:16:00 +0800

    周5第一次提交嗲吗

---
+++ one_teacher_rn/App.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TabBarIOS,
+} from 'react-native';
+// react-navigation
+import { StackNavigator } from 'react-navigation'
+
+const HomeScreen = ({ navigation }) => (
+  <Text onPress={() => { navigation.navigate('Detail') }}>Home</Text>
+)
+
+const DetailScreen = ({navigation}) => (
+  <Text onPress={()=>{navigation.goBack()}}>Detail</Text>
+)
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      headerTitle: 'Home',
+      headerTitleStyle: {
+        color: 'red',
+        textDecorationLine: 'underline'
+      },
+      headerTintColor: 'yellow',
+      headerLeft: <Text onPress={() => { alert(1) }}>左边按钮</Text>,
+      headerBackTitle: 'pop',
+      headerStyle: {
+        backgroundColor: 'yellow',
+        // backfaceVisibility: 'hidden'
+      },
+      gesturesEnabled: false
+    }
+  },
+  Detail: {
+    screen: DetailScreen,
+    navigationOptions: {
+      headerTitle: 'Detail',
+      headerTitleStyle: {
+        color: 'red',
+        textDecorationLine: 'underline'
+      },
+      headerTintColor: 'yellow',
+      headerLeft: <Text onPress={() => { alert(1) }}>左边按钮</Text>,
+      headerBackTitle: 'pop',
+      headerStyle: {
+        backgroundColor: 'yellow',
+        // backfaceVisibility: 'hidden'
+      },
+      gesturesEnabled: true
+    }
+  }
+})
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <RootNavigator />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
